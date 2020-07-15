d90994e63b39a933f53c5bb2d45da39bba3c845e
divya-lalitha <58145201+divya-lalitha@users.noreply.github.com>
2020-07-15 15:35:37 +0530

    Add files via upload

---
+++ Day1/question2.js
@@ -0,0 +1,10 @@
+//let example
+let i = 4;
+if (i > 3) {
+let hello = "say Hello instead";
+console.log(hello);// "say Hello instead"
+ }
+console.log(hello) // hello is not defined
+//Const example
+const c= "hello letsupgrader";
+c = "say hai";// error: Assignment to constant variable. 
